7c9ff8e6580eec51bd8b24c282f922070b38c2dc
MHamzaShahzad <hamzashahzad604@gmail.com>
2019-08-18 13:50:06 +0500

    FirebaseUI phone authentication applied successfully and NavigationDrawer newly created!

--- app/src/main/java/com/itempire/citytocitytravelandroidapp/MainActivity.java
+++ app/src/main/java/com/itempire/citytocitytravelandroidapp/MainActivity.java
@@ -2,13 +2,129 @@ package com.itempire.citytocitytravelandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private Context context;
+
+    private static final int RC_SIGN_IN = 1;
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            initProviders();
+            showSignInOptions();
+        } else {
+            startMainDrawer();
+        }
+
+
     }
+
+    private void getHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+                messageDigest.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(messageDigest.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initProviders() {
+        // Choose authentication providers
+
+        providers = Collections.singletonList(
+                new AuthUI.IdpConfig.PhoneBuilder().build()
+        );
+
+        /*providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );*/
+
+
+    }
+
+    private void showSignInOptions() {
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
+                        .setTheme(R.style.MyTheme)
+                        .build(),
+                RC_SIGN_IN);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Log.e(TAG, "onActivityResult: VALID_REQUEST_CODE");
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                Log.e(TAG, "onActivityResult: VALID_RESULT_CODE");
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    startMainDrawer();
+                }
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                if (response != null && response.getError() != null)
+                    Log.e(TAG, "onActivityResult: Error" + response.getError().getErrorCode());
+                showSignInOptions();
+            }
+        }
+    }
+
+    private void startMainDrawer() {
+        startActivity(new Intent(MainActivity.this, MainDrawerActivity.class));
+        finish();
+    }
+
 }
